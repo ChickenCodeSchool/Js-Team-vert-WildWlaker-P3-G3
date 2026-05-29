9fe4129b30607dffbaf8d77104668b2da62d2a9b
YoanCuervo <cuervo.yoan@gmail.com>
2026-05-30 00:19:23 +0200

    Css / add to component delete & edit

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,9 +5,11 @@ import { RouterProvider, createBrowserRouter } from "react-router";
 
 /* ************************************************************************* */
 
+import TodoList from "./components/ToDoList/TodoList";
 // Import the main app component
 // import App from "./App";
 import Presentation from "./pages/Presentation/Presentation";
+
 // Import additional components for new routes
 // Try creating these components in the "pages" folder
 
@@ -22,6 +24,10 @@ const router = createBrowserRouter([
   {
     path: "/", // The root path
     element: <Presentation />, // Renders the App component for the home page
+  },
+  {
+    path: "/todo",
+    element: <TodoList />,
   },
   // Try adding a new route! For example, "/about" with an About component
 ]);
