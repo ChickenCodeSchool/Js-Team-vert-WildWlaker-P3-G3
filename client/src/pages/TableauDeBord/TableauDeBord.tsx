12f249f51195293f53325d174b151996f16cf0a0
DonDFlamingo <adrianpailhes31@gmail.com>
2026-05-29 16:59:07 +0200

    add side bar desktop and responsive

---
+++ client/src/pages/TableauDeBord/TableauDeBord.tsx
@@ -0,0 +1,11 @@
+import SideBar from "../../components/SideBar/SideBar";
+("../../components/SideBar/SideBar.tsx");
+
+function TableauDeBord() {
+  return (
+    <>
+      <SideBar />
+    </>
+  );
+}
+export default TableauDeBord;
