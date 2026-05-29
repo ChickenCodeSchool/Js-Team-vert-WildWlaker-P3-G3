56ef67a951b44728dc2f32786d221d02ef7b3b9b
YoanCuervo <cuervo.yoan@gmail.com>
2026-05-29 16:28:57 +0200

    fix type task

--- client/src/components/ToDoList/TodoList.tsx
+++ client/src/components/ToDoList/TodoList.tsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 import "TodoList.css";
 
 type Task = {
-  id: number;
-  text: string;
-  completed: boolean;
+  todo_id: number;
+  todo_name: string;
+  todo_is_done: boolean;
 };
 
 function TodoList() {
@@ -15,17 +15,20 @@ function TodoList() {
 
   function handleAddTask() {
     //quand on clique sur '+' setTask crée un nouveau tableau avec toutes les taches qui existent et ajoute un nouvel objet Task qui a un id, un texte et un état false / setAddTask("") remet l'input à vide
-    setTask([...task, { id: Date.now(), text: addTask, completed: false }]);
+    setTask([
+      ...task,
+      { todo_id: Date.now(), todo_name: addTask, todo_is_done: false },
+    ]);
     // Date.now() fonctionne en local en attendant de se brancher au backend
     setAddTask("");
   }
 
   function handleToggleTask(id: number) {
-    // la fonction recoit l'id de la tache concernée. le state parcourt le tableau avec le .map pour chaque taches et si l'id correspond alors ça inverse completed (true devient false ect) et si l'id ne correspond pas alors elle retourne la tache sans aucune modif
+    // la fonction recoit l'id de la tache concernée. le state parcours le tableau avec le .map pour chaque taches et si l'id correspond alors ça inverse completed (true devient false etc) et si l'id ne correspond pas alors elle retourne la tache sans aucune modif
     setTask(
       task.map((t) => {
-        if (t.id === id) {
-          return { ...t, completed: !t.completed };
+        if (t.todo_id === id) {
+          return { ...t, todo_is_done: !t.todo_is_done };
         }
         return t;
       }),
@@ -34,25 +37,25 @@ function TodoList() {
 
   return (
     <div className="todolist-global">
-      <h2>todolist</h2>
+      <h2>Todo List</h2>
       <input
         type="text"
         onChange={(e) => setAddTask(e.target.value)}
         value={addTask}
       />
-      {/*e = evenement déclanché par la frappe de l'utilisataur / e.target l'element html(dans l'input en gros) donc e.target.value c'est le texte qui vie dans l'input*/}
+      {/*e = evenement déclanché par la frappe de l'utilisateur / e.target l'element html(dans l'input en gros) donc e.target.value c'est le texte qui vie dans l'input*/}
       <button type="button" onClick={handleAddTask}>
         +
       </button>
       <ul>
         {task.map((t) => (
-          <li key={t.id}>
+          <li key={t.todo_id}>
             <input
               type="checkbox"
-              checked={t.completed}
-              onChange={() => handleToggleTask(t.id)}
+              checked={t.todo_is_done}
+              onChange={() => handleToggleTask(t.todo_id)}
             />
-            {t.text}
+            {t.todo_name}
           </li>
         ))}
       </ul>
