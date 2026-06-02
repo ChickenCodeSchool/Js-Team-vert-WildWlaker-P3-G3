fa559b35c566f5874b3c935f35afd05ec3207886
Solene Mendes <mendessolene@gmail.com>
2026-06-02 19:04:40 +0200

    Finish css desktop and mobile

--- client/src/components/Budget.tsx
+++ client/src/components/Budget.tsx
@@ -1,6 +1,12 @@
 import "./Budget.css";
 
-import { ArrowDown, ArrowUp, FilePlusCorner } from "lucide-react";
+import {
+  ArrowDown,
+  ArrowUp,
+  FilePlusCorner,
+  Plus,
+  History,
+} from "lucide-react";
 
 function Budget() {
   return (
@@ -16,62 +22,74 @@ function Budget() {
       <section className="monEquilibres">
         <article className="monEquilibresBox">
           <span> tu dois </span>
-          <h2>+1285.00 €</h2>
-          <small> dernier update </small>
+          <h2>+1 285,00 €</h2>
+          <small>
+            <History /> last update : yesterday
+          </small>
         </article>
 
         <article className="total">
           <div className="positif">
-            <ArrowDown size={20} className="lucid" />
+            <ArrowDown size={20} className="lucid" /> <br />
             <span> dépense globales </span>
             <h3> 12 450 € </h3>
           </div>
 
           <div className="negatif">
-            <ArrowUp size={20} className="lucid" />
-            <p> mes dépenses</p>
+            <ArrowUp size={20} className="lucid" /> <br />
+            <span> mes dépenses</span>
             <h3> 5 165 € </h3>
           </div>
         </article>
       </section>
 
       <section className="depensesEquilibres">
-        <article className="depenses">
-          <h5>Dépenses</h5>
-          <div>
-            <span>Billetterie Stripe</span>
-            <span className="positif">+450,00 €</span>
+        <div className="depenses">
+          <div className="mobileDepenses">
+            <h5>Dépenses</h5> <button type="button"> voir plus</button>
           </div>
+          <section>
+            <article>
+              <span>Billetterie Stripe</span>
+              <span className="positif">+450,00 €</span>
+            </article>
 
-          <div>
-            <span>Location Salle</span>
-            <span className="negatif">-1200,00 €</span>
-          </div>
+            <article>
+              <span>Location Salle</span>
+              <span className="negatif">-1200,00 €</span>
+            </article>
 
-          <div>
-            <span>Sponsor TechCorp</span>
-            <span className="positif">-1200,00 €</span>
-          </div>
-        </article>
+            <article>
+              <span>Sponsor TechCorp</span>
+              <span className="positif">-1200,00 €</span>
+            </article>
+          </section>
+        </div>
 
-        <article className="equilibres">
+        <div className="equilibres">
           <h5>Equilibres</h5>
-          <div>
-            <span>Yoan</span>
-            <span className="positif">+450,00 €</span>
-          </div>
+          <section>
+            <article>
+              <span>Yoan</span>
+              <span className="positif">+450,00 €</span>
+            </article>
 
-          <div>
-            <span>Bosila</span>
-            <span className="negatif">-1200,00 €</span>
-          </div>
+            <article>
+              <span>Bosila</span>
+              <span className="negatif">-1200,00 €</span>
+            </article>
 
-          <div>
-            <span>Michel</span>
-            <span className="positif">+1200,00 €</span>
-          </div>
-        </article>
+            <article>
+              <span>Michel</span>
+              <span className="positif">+1200,00 €</span>
+            </article>
+          </section>
+        </div>
       </section>
+
+      <button type="button" className="plusButton">
+        <Plus size={20} />
+      </button>
     </div>
   );
 }
