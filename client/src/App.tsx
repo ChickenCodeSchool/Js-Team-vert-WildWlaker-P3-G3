5ab9a4b351d06e8ba0f0725da8d4d29b6c98bc80
DonDFlamingo <adrianpailhes31@gmail.com>
2026-06-05 12:27:49 +0200

    add files and modal profil

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
-import HomeEvents from "./pages/HomeEvents/HomeEvents";
+// import HomeEvents from "./pages/HomeEvents/HomeEvents";
 // import { Outlet } from "react-router";
 import TableauDeBord from "./pages/TableauDeBord/TableauDeBord";
 import "./App.css";
-import { Home } from "lucide-react";
-import { Outlet } from "react-router";
+// import { Home } from "lucide-react";
+// import { Outlet } from "react-router";
 // import Presentation from "./pages/Presentation/Presentation";
 
 function App() {
