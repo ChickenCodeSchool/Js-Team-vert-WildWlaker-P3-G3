35f3a08abbc50e25cc19d3c7852eaf47ce16c141
YoanCuervo <cuervo.yoan@gmail.com>
2026-06-06 02:31:20 +0200

    Feat:create signalement page with semantic form structure add css and mobile repsonsive

--- client/src/components/ReportUser/ReportEvidence.tsx
+++ client/src/components/ReportUser/ReportEvidence.tsx
@@ -1,3 +1,4 @@
+import { FileUp } from "lucide-react";
 import "./ReportEvidence.css";
 
 function ReportEvidence() {
@@ -5,8 +6,9 @@ function ReportEvidence() {
     <fieldset className="reportEvidence-global">
       <legend>Preuves & Captures</legend>
       <label className="reportEvidence-dropzone">
+        <FileUp size={32} className="reportEvidences-icon" />
         <input type="file" accept=".jpg, .png, .pdf" multiple />
-        Glissez vos fichiers ici ou parcourez
+        Faites glisser ou partager vos fichiers ici.
       </label>
       <p>JPG, PNG ou PDF (Max. 10Mo par fichier)</p>
     </fieldset>
--- client/src/components/ReportUser/ReportType.tsx
+++ client/src/components/ReportUser/ReportType.tsx
@@ -1,3 +1,4 @@
+import { Bug, CalendarX, UserRoundX } from "lucide-react";
 import "./ReportType.css";
 
 function ReportType() {
@@ -6,14 +7,17 @@ function ReportType() {
       <legend>Nature du signalement</legend>
       <div className="reportType-cards">
         <label className="reportType-card">
+          <CalendarX size={24} className="reportType-icon" />
           <input type="radio" name="reportType" value="evenement" />
           Un événement
         </label>
         <label className="reportType-card">
+          <UserRoundX size={24} className="reportType-icon" />
           <input type="radio" name="reportType" value="utilisateur" />
           Un utilisateur
         </label>
         <label className="reportType-card">
+          <Bug size={24} className="reportType-icon" />
           <input type="radio" name="reportType" value="bug" />
           Bug technique
         </label>
