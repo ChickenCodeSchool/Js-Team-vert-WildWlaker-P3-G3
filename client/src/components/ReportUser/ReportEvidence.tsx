05d9ac605c92e0478e82471ceff0f23bcec85f9b
YoanCuervo <cuervo.yoan@gmail.com>
2026-06-08 15:29:39 +0200

    fix: css with new var()

--- client/src/components/ReportUser/ReportEvidence.tsx
+++ client/src/components/ReportUser/ReportEvidence.tsx
@@ -6,11 +6,13 @@ function ReportEvidence() {
     <fieldset className="reportEvidence-global">
       <legend>Preuves & Captures</legend>
       <label className="reportEvidence-dropzone">
-        <FileUp size={32} className="reportEvidences-icon" />
+        <div className="reportEvidence-logoIcon">
+          <FileUp size={32} className="reportEvidences-icon" />
+        </div>
         <input type="file" accept=".jpg, .png, .pdf" multiple />
         Faites glisser ou partager vos fichiers ici.
+        <p>JPG, PNG ou PDF (Max. 10Mo par fichier)</p>
       </label>
-      <p>JPG, PNG ou PDF (Max. 10Mo par fichier)</p>
     </fieldset>
   );
 }
