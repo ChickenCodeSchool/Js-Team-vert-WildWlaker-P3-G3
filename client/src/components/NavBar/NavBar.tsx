fcdcd1547550a330eb936c6010ea55daebcaa69d
YoanCuervo <cuervo.yoan@gmail.com>
2026-06-18 15:56:59 +0200

    add isAdmin to navBar

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -6,13 +6,15 @@ function NavBar() {
   const navbarDesktop =
     location.pathname.startsWith("/homeevents") ||
     location.pathname.startsWith("/events/");
+  const isAdmin = location.pathname.startsWith("/admin/report");
+
   return (
     <>
       <nav className={navbarDesktop ? "desktop" : "mobile"}>
         <div className="logo-sidebar-container">
           <img src={Logo} alt="logo-wedoo" className="logo-sidebar-image" />
           <h2 className="logo-sidebar-h2">
-            WE<i>D</i>OO
+            WE<i>D</i>OO{isAdmin && " Admin"}
           </h2>
         </div>
       </nav>
