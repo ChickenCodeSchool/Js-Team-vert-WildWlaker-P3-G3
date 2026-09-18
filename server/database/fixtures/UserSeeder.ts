802083d059d9926b1efa7f47060d83d0b42a1acd
FARVACQUE-L <laurent.fvq@gmail.com>
2026-09-18 11:41:31 +0200

    Revert "0001 routes"

--- server/database/fixtures/UserSeeder.ts
+++ server/database/fixtures/UserSeeder.ts
@@ -8,11 +8,8 @@ class UserSeeder extends AbstractSeeder {
   run() {
     for (let i = 0; i < 10; i += 1) {
       const fakeUser = {
-        user_name: this.faker.person.fullName(),
-        user_username: this.faker.internet.username(),
-        user_mail: this.faker.internet.email(),
-        user_password: this.faker.internet.password(),
-        user_profile_picture: this.faker.image.avatar(),
+        email: this.faker.internet.email(),
+        password: this.faker.internet.password(),
         refName: `user_${i}`,
       };
 
