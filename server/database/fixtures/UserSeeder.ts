f01fa0361c044bd41be7267dd4fc158f3bccfb28
FARVACQUE-L <laurent.fvq@gmail.com>
2026-09-18 11:21:30 +0200

    style: format UserSeeder with spaces to satisfy biome ci

--- server/database/fixtures/UserSeeder.ts
+++ server/database/fixtures/UserSeeder.ts
@@ -1,24 +1,24 @@
 import AbstractSeeder from "./AbstractSeeder";
 
 class UserSeeder extends AbstractSeeder {
-	constructor() {
-		super({ table: "user", truncate: true });
-	}
+  constructor() {
+    super({ table: "user", truncate: true });
+  }
 
-	run() {
-		for (let i = 0; i < 10; i += 1) {
-			const fakeUser = {
-				user_name: this.faker.person.fullName(),
-				user_username: this.faker.internet.username(),
-				user_mail: this.faker.internet.email(),
-				user_password: this.faker.internet.password(),
-				user_profile_picture: this.faker.image.avatar(),
-				refName: `user_${i}`,
-			};
+  run() {
+    for (let i = 0; i < 10; i += 1) {
+      const fakeUser = {
+        user_name: this.faker.person.fullName(),
+        user_username: this.faker.internet.username(),
+        user_mail: this.faker.internet.email(),
+        user_password: this.faker.internet.password(),
+        user_profile_picture: this.faker.image.avatar(),
+        refName: `user_${i}`,
+      };
 
-			this.insert(fakeUser);
-		}
-	}
+      this.insert(fakeUser);
+    }
+  }
 }
 
 export default UserSeeder;
